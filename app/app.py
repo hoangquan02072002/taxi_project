b9a97a2928c330902651c304ae395212e610471b
hoangquan02072002 <hoangquan02072002@gmai.com>
2024-03-13 17:17:17 +0700

    update1

--- app/app.py
+++ app/app.py
@@ -3,6 +3,9 @@ from sqlalchemy import MetaData
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import os
+from flask_mail import *
+from random import randint
+
 
 app = Flask(__name__)
 application = app
@@ -19,6 +22,16 @@ POSTGRES_URI = f'postgresql://{PGUSER}:{PGPASSWORD}@{ip}/{DATABASE}'
 app.config['SQLALCHEMY_DATABASE_URI'] = POSTGRES_URI
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+mail=Mail(app)
+
+app.config["MAIL_SERVER"]='smtp.gmail.com'
+app.config["MAIL_PORT"]=465
+app.config["MAIL_USERNAME"]='hoangquan02072002@gmail.com'
+app.config['MAIL_PASSWORD']='afsttrhngukehkpx'                    
+app.config['MAIL_USE_TLS']=False
+app.config['MAIL_USE_SSL']=True
+mail=Mail(app)
+otp=randint(000000,999999)
 convention = {
     "ix": 'ix_%(column_0_label)s',
     "uq": "uq_%(table_name)s_%(column_0_name)s",
@@ -48,15 +61,26 @@ def order(id):
   return render_template('order.html',infordriver=infordriver)
 
 
-@app.route('/OrderTaxi/authentication', methods=['POST'])
-def authentication():
-  cms=request.form.get('code')
-  cms_code= Order.query.filter_by(cmc_code=cms).first()
-  if cms_code:
-    return render_template('success.html',cms_code=cms_code)
-  else:
-    return render_template('404.html')
-  
+# @app.route('/OrderTaxi/authentication', methods=['POST'])
+# def authentication():
+#   cms=request.form.get('code')
+#   cms_code= Order.query.filter_by(cmc_code=cms).first()
+#   if cms_code:
+#     return render_template('success.html',cms_code=cms_code)
+#   else:
+#     return render_template('404.html')
 
-
+@app.route('/verify',methods=["POST"])
+def verify():
+    email=request.form['email']
+    msg=Message(subject='OTP',sender='hoangquan02072002@gmail.com',recipients=[email])
+    msg.body=str(otp)
+    mail.send(msg)
+    return render_template('setup-2fa.html')
+@app.route('/validate',methods=['POST'])
+def validate():
+    user_otp=request.form['otp']
+    if otp==int(user_otp):
+        return "<h3>Email verification succesfull</h3>"
+    return "<h3>Please Try Again</h3>"
 
--- app/test_*.py
+++ app/test_*.py
@@ -24,16 +24,16 @@ def test_getall_information_driverTaxi(db_session):
     except IntegrityError:
         db_session.rollback()
 
-def test_authenticate(db_session):
-    try:
-        order = Order(number_car='423742', driver='Driver 1', passenger='Passenger 1', address='Address 1', cmc_code='0207')
-        db_session.add(order)
-        db_session.commit()
-        client.post('/authentication', data={'code': '0207'})
-        result=authentication()
-        assert response.status_code == 200
-    except IntegrityError:
-        db_session.rollback()
+# def test_authenticate(db_session):
+#     try:
+#         order = Order(number_car='423742', driver='Driver 1', passenger='Passenger 1', address='Address 1', cmc_code='0207')
+#         db_session.add(order)
+#         db_session.commit()
+#         client.post('/authentication', data={'code': '0207'})
+#         result=authentication()
+#         assert response.status_code == 200
+#     except IntegrityError:
+#         db_session.rollback()
 
 
 
